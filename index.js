73d89563fa2583e9a8e1a9221f05c29bfc70de34
Nuno Miguel <up201806434@fe.up.pt>
2023-05-05 14:25:46 +0100

    Updates in html css js

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+const mainUrl = "https://754d-2001-8a0-e7fb-d500-c5ca-962-8080-8d1b.ngrok-free.app";
+
+var sensorContainer = document.querySelectorAll(".sensor-container");
+
+fetch(mainUrl + "/measurements?max=1")
+    .then(x=> x.json())
+    .then(res =>{
+        console.log(res);
+    })
+*/
