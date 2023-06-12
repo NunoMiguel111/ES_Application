3b4aeaafa5084f95a409c24ee70e68266219eead
Nuno Miguel <up201806434@fe.up.pt>
2023-06-12 10:35:09 +0100

    Added server api call for map graphic(no hardcoded valueas anymore)

--- sensor.js
+++ sensor.js
@@ -1,91 +1,94 @@
-// Tasks
+// Define server url
+const mainUrl = "http://localhost:8000";
 
-
-// this code compares the two time dates on input 
+// Get references to input elements
 const startDateInput = document.getElementById("start-date-input");
-const endDateInput = document.getElementById("end-date-input")
+const endDateInput = document.getElementById("end-date-input");
 
 let startDateChange = false;
 let endDateChange = false;
 
-startDateInput.addEventListener('change', handleStartDateChange)
-endDateInput.addEventListener('change', handleEndDateChange)
+startDateInput.addEventListener('change', handleStartDateChange);
+endDateInput.addEventListener('change', handleEndDateChange);
 
-function handleStartDateChange(){
+function handleStartDateChange() {
   startDateChange = true;
   checkDatesChanged();
 }
 
-function handleEndDateChange(){
+function handleEndDateChange() {
   endDateChange = true;
   checkDatesChanged();
 }
 
-function checkDatesChanged(){
-  if(startDateChange && endDateChange){
+function checkDatesChanged() {
+  if (startDateChange && endDateChange) {
     startDateChange = false;
     endDateChange = false;
     handleBothDatesChanged();
   }
 }
 
-function handleBothDatesChanged(){
+function handleBothDatesChanged() {
   const startDate = new Date(startDateInput.value);
   const endDate = new Date(endDateInput.value);
 
-  const timeDiff = endDate.getTime() - startDate.getTime()
+  const timeDiff = endDate.getTime() - startDate.getTime();
 
-  if(timeDiff < 0){
-    alert("Starting Date Cannot Be Bigger Than Ending Date")
+  if (timeDiff < 0) {
+    alert("Starting Date Cannot Be Bigger Than Ending Date");
+    return;
   }
 
   // DO SOME LOGIC HERE
-  console.log("We reached here")
-}
+  console.log("We reached here");
 
+  var trajectory = [];
 
-// Define the array of latitudes and longitudes
-var trajectory = [
+  fetch(mainUrl + "/measurements" + "?" + "start_date=" + startDate.toISOString() + "&" + "end_date=" + endDate.toISOString())
+    .then(response => response.json())
+    .then(measurements => {
+      for (let i = 0; i < measurements.length; i++) {
+        trajectory.push([parseFloat(measurements[i].location[0]), parseFloat(measurements[i].location[1])]);
+      }
 
-    [41.178463256862216, -8.595062196985669],
-    [41.178447369725745, -8.593731843991833],
-    [41.177288548542016, -8.593549177761107]
-  
-  ];
-  
-  // Initialize the map
-  var map = L.map('map').setView(trajectory[0], 10); // Set the initial view and zoom level
-  
-  // Create and add the OpenStreetMap tile layer to the map
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-  }).addTo(map);
-  
-  var markers = L.featureGroup();
-  
-  for(var i = 0; i< trajectory.length; i++){
-    var marker = L.circleMarker(trajectory[i], {
-      color : "red",
-      fillColor: "red",
-      fillOppacity: 1,
-      radius: 8,
-  
+      // Initialize the map
+      var map = L.map('map').setView(trajectory[0], 10); // Set the initial view and zoom level
+
+      // Create and add the OpenStreetMap tile layer to the map
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+      }).addTo(map);
+
+      var markers = L.featureGroup();
+
+      for (var i = 0; i < trajectory.length; i++) {
+        var marker = L.circleMarker(trajectory[i], {
+          color: "red",
+          fillColor: "red",
+          fillOpacity: 1,
+          radius: 8,
+        });
+        markers.addLayer(marker);
+      }
+
+      // Add markers to map
+      markers.addTo(map);
+
+      // Create a Polyline to represent the trajectory
+      var polyline = L.polyline(trajectory, {
+        color: 'blue', // Adjust the color of the line as needed
+        weight: 2 // Adjust the thickness of the line as needed
+      }).addTo(map);
+
+      // Fit the map bounds to the polyline
+      map.fitBounds(polyline.getBounds());
     })
-    markers.addLayer(marker);
-  }
-  
-  // Add markers to map
-  markers.addTo(map);
-  
-  // Create a Polyline to represent the trajectory
-  var polyline = L.polyline(trajectory, {
-    color: 'blue', // Adjust the color of the line as needed
-    weight: 2 // Adjust the thickness of the line as needed
-  }).addTo(map);
-  
-  // Fit the map bounds to the polyline
-  map.fitBounds(polyline.getBounds());
+    .catch(error => {
+      console.error("Error fetching measurements:", error);
+    });
 
+  // Rest of your code
   const data = {
     datasets: [{
       label: 'Air Temperature',
@@ -130,4 +133,8 @@ var trajectory = [
   var canvas = document.getElementById("myChart");
 
   const scatterPlot = new Chart(canvas, config);
-  
+}
+
+
+
+
